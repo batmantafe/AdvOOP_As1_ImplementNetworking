14ed71ebca131b63dc5e8fc87ec967db05ada33d
Duane Reymond <duane.reymond@gmail.com>
2018-03-23 14:36:23 +1100

    AIAgent script added and working.

---
+++ Assets/Scripts/Enemy/AIAgent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIAgent : MonoBehaviour
+{
+    public Transform player;
+
+    private NavMeshAgent nav;
+    
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        NavTarget();
+    }
+
+    void NavTarget()
+    {
+        if (player != null)
+        {
+            // Set destination to Player's position
+            nav.SetDestination(player.position);
+        }
+    }
+}
