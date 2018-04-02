92a4d1791a7a5f68df8eeb12e587111719711181
Duane Reymond <duane.reymond@gmail.com>
2018-04-02 13:41:04 +1000

    In Player2Input script, Player1 and Player2 are now differentiated.

--- Assets/Scripts/Player2/Player2Input.cs
+++ Assets/Scripts/Player2/Player2Input.cs
@@ -7,24 +7,14 @@ using UnityEngine.Networking;
 
 public class Player2Input : NetworkBehaviour
 {
+    public GameObject body;
+    private MouseLook player2MouseLook;
+    private Movement player2Movement;
+    private Vector3 spawn1;
+
     void Start()
     {
-        Cursor.visible = false;
-
-        AudioListener audioListener = GetComponentInChildren<AudioListener>();
-        Camera camera = GetComponentInChildren<Camera>();
-
-        if (isLocalPlayer)
-        {
-            camera.enabled = true;
-            audioListener.enabled = true;
-        }
-        else
-        {
-            camera.enabled = false;
-            audioListener.enabled = false;
-        }
-
+        LocalPlayerStartSetup();
     }
 
     void Update()
@@ -49,6 +39,56 @@ public class Player2Input : NetworkBehaviour
         {
             SceneManager.LoadScene("Game");
         }
+    }
+    #endregion
+
+    #region Local Player's Start Setup
+    void LocalPlayerStartSetup()
+    {
+        Cursor.visible = false;
+
+        AudioListener audioListener = GetComponentInChildren<AudioListener>();
+        Camera camera = GetComponentInChildren<Camera>();
+
+        player2MouseLook = GetComponent<MouseLook>();
+        player2Movement = GetComponent<Movement>();
+
+        spawn1 = new Vector3(5,2,0);
+
+        if (isLocalPlayer)
+        {
+            camera.enabled = true;
+            audioListener.enabled = true;
+        }
+
+        if (!isLocalPlayer)
+        {
+            camera.enabled = false;
+            audioListener.enabled = false;
+        }
+
+        // IF this Player starts at Player 1's position
+        if (transform.position == spawn1)
+        {
+            Cursor.visible = true;
+
+            player2MouseLook.enabled = false;
+            player2Movement.enabled = false;
+
+            body.GetComponent<MeshRenderer>().enabled = false;
+            body.GetComponent<SphereCollider>().enabled = false;
+
+            CharacterController charCont = GetComponent<CharacterController>();
+            charCont.enabled = false;
+
+            transform.position = new Vector3(8,300,0);
+            transform.Rotate(90,0,180);
+
+            camera.orthographic = true;
+            camera.orthographicSize = 91;
+        }
+
+        
     }
     #endregion
 
