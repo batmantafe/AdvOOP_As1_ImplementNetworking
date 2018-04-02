9b82b0a68afc2df3fa539b1f270a44a40b90ea51
Duane Reymond <duane.reymond@gmail.com>
2018-04-02 17:35:35 +1000

    Level spawning in from NetworkManager... still need to sync Tile vars.

---
+++ Assets/Scripts/Player2/SpawnLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class SpawnLevel : NetworkBehaviour
+{
+    public GameObject levelPrefab;
+    GameObject levelInstantiated;
+
+    private bool levelDone;
+
+    void Start()
+    {
+        CmdSyncLevel();
+    }
+
+    [Command]
+    void CmdSyncLevel()
+    {
+        levelDone = false;
+
+        if (isServer && levelDone == false)
+        {
+            Debug.Log("is Server!");
+
+            levelInstantiated = Instantiate(levelPrefab);
+
+            NetworkServer.Spawn(levelInstantiated);
+
+            levelDone = true;
+        }
+    }
+}
